474a59a6e862072fb8e071098b8407130d0c478b
keqings <88179044+keqings@users.noreply.github.com>
2021-07-31 00:53:47 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,6 +2,17 @@ import discord
 from discord.ext import commands
 import os
 
+@client.event
+async def on_ready():
+
+  # [discord.Status.online = 온라인],[discord.Status.idle = 자리비움],[discord.Status.dnd = 다른용무],[discord.Status.offline = 오프라인]
+  await client.change_presence(status=discord.Status.online)
+
+  await client.change_presence(activity=discord.Game(name="게임"))
+  #await client.change_presence(activity=discord.Streaming(name="스트림 방송중", url='링크'))
+  #await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="노래 듣는중"))
+  #await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="영상 시청중"))
+  
 client = commands.Bot(command_prefix = '청아 ')
 
 @client.command()
@@ -32,17 +43,7 @@ async def 롤패치노트(ctx):
 async def 원신나무위키(ctx):
     await ctx.send('https://namu.wiki/w/%EC%9B%90%EC%8B%A0')   
                           
-@client.event
-async def on_ready():
 
-  # [discord.Status.online = 온라인],[discord.Status.idle = 자리비움],[discord.Status.dnd = 다른용무],[discord.Status.offline = 오프라인]
-  await client.change_presence(status=discord.Status.online)
-
-  await client.change_presence(activity=discord.Game(name="게임"))
-  #await client.change_presence(activity=discord.Streaming(name="스트림 방송중", url='링크'))
-  #await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="노래 듣는중"))
-  #await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="영상 시청중"))
-  
   print("봇 이름:",client.user.name,"봇 아이디:",client.user.id,"봇 버전:",discord.__version__)
 
 
