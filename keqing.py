144f50f271102ff13c186b726684bff9384787c2
keqings <88179044+keqings@users.noreply.github.com>
2021-07-30 20:33:43 +0900

    Update keqing.py

--- keqing.py
+++ keqing.py
@@ -18,4 +18,4 @@ async def on_ready():
   print("봇 이름:",client.user.name,"봇 아이디:",client.user.id,"봇 버전:",discord.__version__)
 
 
-client.run(os.environ[ODY4ODQ1OTAzOTk2NDExOTI0.YP1lzQ.lPhhbjBTKTQvnDjJ_-02z317vEs])
+client.run(os.environ['token'])
