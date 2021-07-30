0d42c4b6792650b69383a46168e40c787e4d5616
keqings <88179044+keqings@users.noreply.github.com>
2021-07-31 01:12:46 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,6 +2,32 @@ import discord
 from discord.ext import commands
 import os
 
+client = commands.Bot(command_prefix = '청아 ')
+
+@client.command()
+async def 안녕(ctx):
+    await ctx.send('안녕여행자')
+
+@.command()
+async def 도움말(ctx) :
+    embed = discord.Embed(title = "도움말",
+    description = "리월칠성 청이 사용하는 방법입니다.\n\n**청아 안녕**\청이랑 인사할 수 있습니다.\n\n**청아 원신**\원신 공식 유튜브입니다.\n\n**청아 지도**\n티바트 지역 전체 맵을 볼 수 있습니다.", color = 0x62c1cc)
+    embed.add_field(name = "필드(Field)의 Name", value = "필드(Field)의 Value")
+    embed.set_footer(text = f"{ctx.message.author.name}", icon_url = ctx.message.author.avatar_url)
+    await ctx.send(embed = embed)
+
+@client.command()
+async def 원신(ctx):
+    await ctx.send('https://www.youtube.com/channel/UCcum1rCJ5GJeQ_xv0xrohqg')
+
+@client.command()
+async def 지도(ctx):
+    await ctx.send('https://webstatic-sea.mihoyo.com/app/ys-map-sea/index.html?lang=ko-kr#/map/2?shown_types=5,194&center=82.00,-1169.00&zoom=-2.00')
+
+@client.command()
+async def 롤패치노트(ctx):
+    await ctx.send('https://kr.leagueoflegends.com/ko-kr/news/tags/patch-notes')   
+    
 @client.event
 async def on_ready():
 
@@ -15,29 +41,5 @@ async def on_ready():
   
   print("봇 이름:",client.user.name,"봇 아이디:",client.user.id,"봇 버전:",discord.__version__)
 
-client = commands.Bot(command_prefix = '청아 ')
 
-@client.command()
-async def 안녕(ctx):
-    await ctx.send('안녕여행자')
-
-@client.command()
-async def 도움말(ctx) :
-    embed = discord.Embed(title = "도움말",
-    description = "리월칠성 청이 사용하는 방법입니다. \n\n**청아 안녕**\청이랑 인사할 수 있습니다.\n\n**청아 원신**\n원신 공식 유튜브입니다.\n\n**청아 지도**\n티바트 지역 전체 맵을 볼 수 있습니다.\n\n**청아 원신나무위키**\n원신 나무위키입니다.", color = 0x62c1cc)   
-    embed.set_footer(text = f"{ctx.message.author.name}", icon_url = ctx.message.author.avatar_url)
-    await ctx.send(embed = embed)
-
-@client.command()
-async def 원신(ctx):
-    await ctx.send('https://www.youtube.com/channel/UCcum1rCJ5GJeQ_xv0xrohqg')
-
-@client.command()
-async def 지도(ctx):
-    await ctx.send('https://webstatic-sea.mihoyo.com/app/ys-map-sea/index.html?lang=ko-kr#/map/2?shown_types=5,194&center=82.00,-1169.00&zoom=-2.00')
-
-@client.command()
-async def 원신나무위키(ctx):
-    await ctx.send('https://namu.wiki/w/%EC%9B%90%EC%8B%A0')   
-                          
 client.run(os.environ['token'])
