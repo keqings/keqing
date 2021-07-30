56658d17108404ba7acb2310257174865ac53854
keqings <88179044+keqings@users.noreply.github.com>
2021-07-31 01:08:26 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -24,7 +24,7 @@ async def 안녕(ctx):
 @client.command()
 async def 도움말(ctx) :
     embed = discord.Embed(title = "도움말",
-    description = "리월칠성 청이 사용하는 방법입니다. \n\n**청아 안녕**\청이랑 인사할 수 있습니다.\n\n**청아 원신**\n원신 공식 유튜브입니다.\n\n**청아 지도**\n티바트 지역 전체 맵을 볼 수 있습니다.\n\n**청아 롤패치노트**\n롤 패치노트를 볼 수 있습니다.\n\n**청아 원신나무위키**\n원신 나무위키입니다.", color = 0x62c1cc)   
+    description = "리월칠성 청이 사용하는 방법입니다. \n\n**청아 안녕**\청이랑 인사할 수 있습니다.\n\n**청아 원신**\n원신 공식 유튜브입니다.\n\n**청아 지도**\n티바트 지역 전체 맵을 볼 수 있습니다.\n\n**청아 원신나무위키**\n원신 나무위키입니다.", color = 0x62c1cc)   
     embed.set_footer(text = f"{ctx.message.author.name}", icon_url = ctx.message.author.avatar_url)
     await ctx.send(embed = embed)
 
@@ -35,10 +35,6 @@ async def 원신(ctx):
 @client.command()
 async def 지도(ctx):
     await ctx.send('https://webstatic-sea.mihoyo.com/app/ys-map-sea/index.html?lang=ko-kr#/map/2?shown_types=5,194&center=82.00,-1169.00&zoom=-2.00')
-
-@client.command()
-async def 롤패치노트(ctx):
-    await ctx.send('https://kr.leagueoflegends.com/ko-kr/news/tags/patch-notes')   
 
 @client.command()
 async def 원신나무위키(ctx):
